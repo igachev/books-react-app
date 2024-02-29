6638f6e33ab7f5c306de843ea1fe1af4b0703215
igachev <ivophoenix1135@gmail.com>
2024-02-29 16:08:40 +0200

    added useEffect() to get all books when App component is mounted

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import './App.css'
 import Header from './components/Header'
@@ -6,10 +6,27 @@ import Logo from './components/Logo'
 import Navbar from './components/NavBar'
 import List from './components/List'
 import Main from './components/Main'
+import * as bookService from './services/bookService.js'
 
 function App() {
   
   const [books,setBooks] = useState([])
+  const [error,setError] = useState("")
+
+  async function getBooks() {
+    try {
+      const result = await bookService.getBooks()
+     // console.log(result)
+      setBooks(result)
+    } catch (err) {
+     // console.log(err)
+      setError(err.message)
+    }
+  }
+
+  useEffect(() => {
+    getBooks()
+  },[])
 
   return (
     <div className='container'>
